f3b5de66a500cedfaa3bcb884bfb16a3215b2659
caa1211 <caa@livemail.tw>
2014-06-01 05:20:11 +0800

    add demo

---
+++ js/demo.js
@@ -0,0 +1,13 @@
+var demoList = [{
+    "name": "Castle",
+    "path": "demo/castle.oa"
+}, {
+    "name": "Chair",
+    "path": "demo/chair.oa"
+}, {
+    "name": "Hourse",
+    "path": "demo/hourse.oa"
+}, {
+    "name": "Bath",
+    "path": "demo/bath.oa"
+}];
